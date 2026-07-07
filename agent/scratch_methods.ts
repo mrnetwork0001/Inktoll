2ad837d966cfcc931308cd8a34301b657c42e6dd
mrnetwork0001 <emma40mike@gmail.com>
2026-07-07 05:36:12 +0100

    chore: cleanup unused test and legacy files

--- agent/scratch_methods.ts
+++
@@ -1,3 +0,0 @@
-import { GatewayClient } from '@circle-fin/x402-batching/client';
-
-console.log('Methods:', Object.getOwnPropertyNames(GatewayClient.prototype));
--- agent/scratch_scheme.js
+++
@@ -1,9 +0,0 @@
-const { BatchSettlementEvmScheme } = require('@x402/evm');
-const s = new BatchSettlementEvmScheme({ 
-  chainId: 5042002, 
-  verifyingContract: '0x0077777d7EBA4688BDeF3E311b846F25870A19B9',
-  domainName: 'GatewayWalletBatched',
-  domainVersion: '1'
-});
-console.log('Domain:', s.getDomain());
-console.log('Types:', s.getTypes());
